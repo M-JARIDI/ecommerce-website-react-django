f23fb5169e6bb1537febfb74d4ad7e5fcdb0a9e2
mustapha jaridi <mustaphajaridi@gmail.com>
2021-09-18 12:24:07 +0200

    Serializing data

--- base/views.py
+++ base/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 # from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from .models import Product
 from .products import products
+from .serializers import ProductSerializer
 
 # Create your views here.
 
@@ -13,13 +16,18 @@ def getRoutes(request):
 @api_view(['GET'])
 def getProducts(request):
     # return JsonResponse(products, safe=False)
-    return Response(products)
+    products = Product.objects.all() # returns back all the products from our database
+    serializer = ProductSerializer(products, many=True) # that means that we have multipls products
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProduct(request, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
-    return Response(product)
+    product = Product.object.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+    # product = None
+    # for i in products:
+    #     if i['_id'] == pk:
+    #         product = i
+    #         break
+    # return Response(product)
