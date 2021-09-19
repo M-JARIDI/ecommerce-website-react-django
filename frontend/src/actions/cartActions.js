cde2651e91b33eddde5a8ec35d4276d666f08506
mustapha jaridi <mustaphajaridi@gmail.com>
2021-09-19 13:11:51 +0200

    add to cart functionality

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -16,6 +16,24 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
       qty,
     },
   });
+  console.log(`test action`);
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = (id, qty) => async (dispatch, getState) => {
+  const { data } = await axios.get(`/api/products/${id}`);
+
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: {
+      product: data._id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      counterInStock: data.counterInStock,
+      qty,
+    },
+  });
 
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
