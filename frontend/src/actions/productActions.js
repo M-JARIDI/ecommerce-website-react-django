239c4daf8d5c99fa69f2e9cbce3f67cd00fd1d3e
mustapha jaridi <mustaphajaridi@gmail.com>
2021-09-22 10:46:24 +0200

    user login reducer & action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -16,8 +16,8 @@ export const listProducts = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_LIST_FAIL,
-      payload: error.response?.data?.message
-        ? error.response.data.message
+      payload: error.response?.data?.detail
+        ? error.response.data.detail
         : error.message,
     });
   }
@@ -31,8 +31,8 @@ export const listProductDetails = (id) => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_DETAILS_FAIL,
-      payload: error.response?.data?.message
-        ? error.response.data.message
+      payload: error.response?.data?.detail
+        ? error.response.data.detail
         : error.message,
     });
   }
